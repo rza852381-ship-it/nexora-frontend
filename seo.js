74bd9658581815d3b41962354953278d3db5cad4
rza852381-ship-it <rza852381@gmail.com>
2026-03-13 11:52:52 +0300

    Create seo.js

---
+++ seo.js
@@ -0,0 +1,12 @@
+function loadSEO(){
+
+document.title="Nexora Platform"
+
+let meta=document.createElement("meta")
+
+meta.name="description"
+meta.content="Nexora AI Ecommerce Platform"
+
+document.head.appendChild(meta)
+
+}
