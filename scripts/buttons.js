4e1de4a23252d040eed2be698a5f63720b1df1d6
rza852381-ship-it <rza852381@gmail.com>
2026-03-14 09:32:52 +0300

    Create buttons.js

---
+++ scripts/buttons.js
@@ -0,0 +1,19 @@
+function openDashboard(){
+window.location.href="dashboard.html"
+}
+
+function openCampaign(){
+window.location.href="campaign-ai.html"
+}
+
+function openProfits(){
+window.location.href="profits.html"
+}
+
+function openPricing(){
+window.location.href="pricing.html"
+}
+
+function logout(){
+window.location.href="index.html"
+}
